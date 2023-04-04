906ed6a38c4c7389e3c0dd355159cf71cc99dad5
s-121-16 <s-121-16@gumiforest.ae.krakow.pl>
2023-04-04 14:41:33 +0200

    init part 2

---
+++ company.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            title: 'UNIKPOL S.A.',
+            street: 'ul. Ukryta 5',
+            address: '02-114 Rawa Mazowiecka',
+            phone_numbers: 'tel. 22 345 90 21, 888 213 992',
+            website: 'http://www.unikpol.pl'
+        }
+    }
+})
